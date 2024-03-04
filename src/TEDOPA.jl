55f0d64c8cfc36a80bdd3c9490d5aeb176fc4ac8
Davide Ferracin <davide.ferracin@protonmail.com>
2024-03-04 02:45:11 +0100

    Incorporate chain-mapping scripts from MC repo
    Add the thermofield and TF-TEDOPA chain-mapping scripts used in the
    Markovian closure project repository.

--- src/TEDOPA.jl
+++ src/TEDOPA.jl
@@ -5,9 +5,19 @@ using PolyChaos
 using QuadGK
 using DataFrames
 using CSV
+using DelimitedFiles
+using JSON
 
 export getchaincoefficients, thermalisedJ, chainmapcoefficients
 
 include("tedopa.jl")
+
+export chainmapping_thermofield
+
+include("thermofield.jl")
+
+export chainmapping_tftedopa
+
+include("tftedopa.jl")
 
 end
