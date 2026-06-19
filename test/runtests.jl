e42884f7d8d2db1f84e0ae456210f111913b0ec6
Davide Ferracin <davide.ferracin@protonmail.com>
2026-06-19 17:58:54 +0200

    test: check TEDOPA also with s=1 Ohmic sdf

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,42 @@
 using Test
 using TEDOPA, QuadGK
 
-@testset "TEDOPA with truncated Ohmic spectral density" begin
+@testset "TEDOPA with truncated Ohmic (s=1) spectral density" begin
+    rtol = 1e-6
+    xc = 4
+    s = 1
+    ohmic_sdf = "x^a[1]"
+    chainlength = 1_000
+
+    dict = Dict(
+        "environment" => Dict(
+            "spectral_density_parameters" => [s],
+            "spectral_density_function" => ohmic_sdf,
+            "domain" => [0.0, xc],
+            "temperature" => 0,
+        ),
+        "chain_length" => chainlength,
+        "PolyChaos_nquad" => 2*chainlength,
+    )
+
+    env = chainmapping_tedopa(dict)
+
+    # (Here we also test the ability to directly call `env` as if it were a function.)
+    @test_throws DomainError env(-1.0)
+
+    intJ, err_intJ = quadgk(env, domain(env)...)
+    @test first(couplings(env))^2 ≈ intJ atol=err_intJ
+
+    # Exact values taken from Chin, Rivas, Huelga and Plenio, J. Math. Phys. 51 (9) 092109.
+    @test frequencies(env) ≈
+        [xc/2 * (1 + s^2 / ((s + 2n)*(s+2n+2))) for n in 0:(chainlength - 1)] rtol=rtol
+    @test couplings(env)[2:end] ≈ [
+        xc*(1+n)*(1+s+n)/((s+2+2n)*(3+s+2n)) * sqrt((3+s+2n)/(1+s+2n)) for
+        n in 0:(chainlength - 2)
+    ] rtol=rtol
+end
+
+@testset "TEDOPA with truncated Ohmic (s=2) spectral density" begin
     rtol = 1e-6
     xc = 4
     s = 2
